cd3f0df8f235ec5880681fb30503592c95935ecb
Alex Tribbey <alx_036@yahoo.com>
2025-01-24 11:48:47 -0600

    Step 5: Configuring Vite to build your extension properly

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [{ src: "manifest.json", dest: "." }],
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: "index.html",
+        "service-worker": "service-worker.js",
+        "content-script": "content-script.js",
+      },
+      output: {
+        entryFileNames: "[name]/[name].js",
+        chunkFileNames: "[name]/[name].js",
+        assetFileNames: "[name]/[name].[ext]",
+      },
+    },
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+  root: ".",
+});
